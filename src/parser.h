30fd7d9f3a572fce53b2f32bedbe911bbb2bc7d0
Filipe RdF Oliveira <filiperdf@Filipes-MBP.lan>
2015-02-26 01:16:31 +0000

    criados os ficheiros .h mediante a estrutura proposta pelos professores no enunciado

---
+++ src/parser.h
@@ -0,0 +1,12 @@
+/*
+ *  * Copyright (C) Carlos Sá, Filipe Oliveira, Sérgio Caldas
+ *  * Copyright (C) Laboratórios Informática III, Universidade do Minho, 2015
+ *  */
+
+
+#ifndef _PARSER_H_INCLUDED_
+#define _PARSER_H_INCLUDED_
+
+
+#endif /* _PARSER_H_INCLUDED_ */
+
---
+++ src/transaction.h
@@ -0,0 +1,12 @@
+/*
+ *  * Copyright (C) Carlos Sá, Filipe Oliveira, Sérgio Caldas
+ *  * Copyright (C) Laboratórios Informática III, Universidade do Minho, 2015
+ *  */
+
+
+#ifndef _TRANSACTION_H_INCLUDED_
+#define _TRANSACTION_H_INCLUDED_
+
+
+#endif /* _TRANSACTION_H_INCLUDED_ */
+
