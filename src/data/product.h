75799c695997a2e6d789c96638cd5f5bb669ba35
Filipe RdF Oliveira <filiperdf@Filipes-MBP.lan>
2015-03-03 23:29:22 +0000

    adicionei os tipos de funções ao .h de product

--- src/data/product.h
+++ src/data/product.h
@@ -7,7 +7,25 @@
 #ifndef _PRODUCT_H_INCLUDED_
 #define _PRODUCT_H_INCLUDED_
 
-typedef struct product* Product;
+typedef struct product * Product;
+
+Product newProduct ( void );
+
+void initProduct ( Product );
+
+void setProduct ( char, char, char[4] );
+
+char getFirstLetter ( Product );
+
+char getSecondLetter ( Product );
+
+char* getNumbers ( Product );
+
+int equals ( Product , Product );
+
+void destroyProduct ( Product );
+
+char* toString ( Product );
 
 #endif /* _PRODUCT_H_INCLUDED_ */
 
