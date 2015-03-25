5e199551f01e89ee6f49cb62bfa15f5bcb3bf8c0
Filipe Oliveira <filipeoliveira@Filipes-MBP.lan>
2015-03-25 10:09:36 +0000

    continue na bilioteca accountingProduct

--- src/accountingProduct.c
+++ src/accountingProduct.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
-#include "client.h"
+#include "accountingProduct.h"
 
 struct accountingProduct {
   int NSales;
@@ -10,30 +10,34 @@ struct accountingProduct {
 };
 
 Accounting newAccountingProduct ( void ){
-  AccountingProduct accountingNew = ( AccountingProduct ) malloc ( sizeof ( struct accountingProduct ));
-  memset ( (clientNew), '\0', ( sizeof ( struct client)) );
-  return clientNew;
+  AccountingProduct accountingProductNew = ( AccountingProduct ) malloc ( sizeof ( struct accountingProduct ));
+  memset ( (accountingProductNew), '\0', ( sizeof ( struct accountingProduct)) );
+  return ( accountingProductNew );
 }
 
-void setClient ( Client clientToSet, char fstLetter, char sndLetter, char setNumbers[3] ){
+void setAccountingProduct ( AccountingProduct acProdToSet, int n_Sales, float n_Billed, int p_Sales, float p_Billed ){
   clientToSet->firstLetter=fstLetter;
   clientToSet->secondLetter=sndLetter;
   strcpy ( clientToSet->numbers , setNumbers );
 }
 
-char getFirstLetter ( Client  getClient ) { 
-  return getClient->firstLetter;
+int getNSales ( AccountingProduct getAccountingProduct ) {
+  return ( getAccountingProduct->Nsales );
 }
 
-char getSecondLetter ( Client getClient ) {
-  return getClient->secondLetter;
+int getPSales ( AccountingProduct getAccountingProduct ) {
+  return ( getAccountingProduct->PSales );
 }
 
-char* getNumbers ( Client getClient ) {
-  return getClient->numbers;
+float getNBilled ( AccountingProduct getAccountingProduct ) {
+  return ( getAccountingProduct->NBilled );
 }
 
-int equals (Client isEqual1, Client isEqual2 ){
-  return ( isEqual1->firstLetter==isEqual2->firstLetter) & (isEqual1->secondLetter==isEqual2->secondLetter) & ( strcmp (isEqual1->numbers , isEqual2->numbers));
+float getPBilled ( AccountingProduct getAccountingProduct ) {
+  return ( getAccountingProduct->PBilled );
+}
+
+float getTotalBilled ( AccountingProduct getAccountingProduct ) {
+  return ( getAccountingProduct->NBilled + getAccountingProduct->PBilled );
 }
 
